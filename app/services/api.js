9f2adda7afb08061fc747877ab35f4c83d7ed7db
Muhammad Talha Rehman <miantalhapaf@gmail.com>
2024-12-24 13:31:04 +0500

    api header change

--- app/services/api.js
+++ app/services/api.js
@@ -2,6 +2,9 @@ import axios from 'axios';
 
 const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
+  headers: {
+    'ngrok-skip-browser-warning': 'true'
+}
 });
 
 export const getProducts = async () => {
