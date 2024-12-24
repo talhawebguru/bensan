8a8403315708eaccde5c79c78d7b49de2e7b9c8d
Muhammad Talha Rehman <miantalhapaf@gmail.com>
2024-12-24 13:51:08 +0500

    next.config change

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['localhost'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'localhost',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'ca86-2-50-148-216.ngrok-free.app',
+        port: '',
+        pathname: '/**',
+      },
+    ],
   },
 };
 
