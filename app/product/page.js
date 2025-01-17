ec9d7e0f3085b9125205600b72f8d74879eb5d52
Muhammad Talha Rehman <miantalhapaf@gmail.com>
2025-01-17 17:26:17 +0500

    animation overflow added and category grid adjust

--- app/product/page.js
+++ app/product/page.js
@@ -120,7 +120,7 @@ const ProductsPage = () => {
         </div>
         <div>
           <motion.div 
-            className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-6"
+            className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-6 overflow-hidden"
             variants={containerVariants}
             initial="hidden"
             animate="visible"
